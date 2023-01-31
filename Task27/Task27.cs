76b2204d367d63d9f84247bb1cc70336e5b98f66
Valeriy2077 <84186810+Valeriy2077@users.noreply.github.com>
2023-02-01 01:01:18 +0300

    Task27

--- Task27/Task27.cs
+++ Task27/Task27.cs
@@ -6,23 +6,16 @@
 
 // 9012 -> 12
 
-Console.Write("Введите число N: ");
-int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
 
-  int SumNumber(int numberN)
-  {
-    
-    int counter = Convert.ToString(numberN).Length;
-    int advance = 0;
-    int result = 0;
+while (i > 0)
+{
 
-    for (int i = 0; i < counter; i++){
-      advance = numberN - numberN % 10;
-      result = result + (numberN - advance);
-      numberN = numberN / 10;
-    }
-   return result;
-  }
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
 
-int sumNumber = SumNumber(numberN);
-Console.WriteLine("Сумма цифр: " + sumNumber);
+}
+Console.WriteLine("сумма всех цифр в числе: " + sum);
